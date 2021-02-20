89d5ccf45e7201cb9954949dc14b5af3dfe095a1
Jean Enchanted <x22me22x@gmail.com>
2021-02-20 14:07:21 +0000

    Some bugs need fixing

--- commands/music/play.js
+++ commands/music/play.js
@@ -3,7 +3,11 @@ module.exports = {
   aliases: ["start", "p", "add"],
   code: `
 $title[Added to queue:]
-$description[$playSong[$message;7d;yes;Couldn't play the current song!]]
+$description[
+	$replaceText[
+	$playSong[$message;7d;yes;Couldn't play the current song!]
+	;\];\\]]
+]
 $onlyIf[$voiceID!=;{color:$getRoleColor[$highestRole[784688234672160788]]}{description:You aren’t in voice channel.Please enter and try again.}]
 $onlyIf[$message!=;{color:$getRoleColor[$highestRole[784688234672160788]]}{description:Enter a link or song name.}]
 
