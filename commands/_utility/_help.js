41f853dc8a81666d85c7eada931a72bbf0ae7ec5
Jean <x22me22x@outlook.com>
2021-03-14 19:39:17 +0300

    back to square one

--- commands/_utility/_help.js
+++ commands/_utility/_help.js
@@ -117,24 +117,11 @@ $createObject[{
 }]
 
 `,
-    /* helpFunc: {
-        module: (number) => {
-            return `$title[
-                $toLocaleUppercase[$getObjectProperty[a${number}name]] | Commands
-                ]
-        
-                $description[
-                    \`\`\`$getObjectProperty[a${number}commands]\`\`\`
-                    Visit the Study Group [website\\](https://akis.glitch.me) or check out the [docs\\](https://akis.glitch.me/docs.html).
-                ]`
-        },
-        moduleList: (emoji, number) => {
-            return `
-            $addField[${emoji} $toLocaleUppercase[$getObjectProperty[a${number}name]]; \`$getObjectProperty[prefix]help $getObjectProperty[a${number}name]\`;yes]
-            `
-        }
-    } */
-    module: function(number) {
+}
+
+
+const helpFunc = {
+    module: (number) => {
         return `$title[
             $toLocaleUppercase[$getObjectProperty[a${number}name]] | Commands
             ]
@@ -144,6 +131,9 @@ $createObject[{
                 Visit the Study Group [website\\](https://akis.glitch.me) or check out the [docs\\](https://akis.glitch.me/docs.html).
             ]`
     },
+    moduleList: (emoji, number) => {
+        return `
+        $addField[${emoji} $toLocaleUppercase[$getObjectProperty[a${number}name]]; \`$getObjectProperty[prefix]help $getObjectProperty[a${number}name]\`;yes]
+        `
+    }
 }
-
-
